4a5790e60fbb60de40a4a4b141881710b947f09f
Rejy Karinga <rejykaringa2000@gmail.com>
2026-06-16 23:09:11 +0530

    url check

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
+// const BASE_URL = 'https://nikkahmatch.in';
+// const BASE_URL = 'https://wishigrove.com';
 const BASE_URL = 'http://43.205.99.214';
-// const BASE_URL = 'http://localhost:8000';
+const FALLBACK_URL = 'http://43.205.99.214';
 
 export const CONFIG = {
     BASE_URL: BASE_URL,
     API_URL: `${BASE_URL}/api`,
+    FALLBACK_URL: FALLBACK_URL,
 };
 
 export default CONFIG;
